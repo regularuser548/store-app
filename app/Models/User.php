e26876ed5afefb696eb9605116b79a774a4c55db
regularuser548 <regularuser548@tutanota.com>
2024-08-25 19:27:20 +0300

    Add Vanilo Framework

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,41 +7,50 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+//class User extends Authenticatable
+//{
+//    use HasFactory, Notifiable;
+//
+//    /**
+//     * The attributes that are mass assignable.
+//     *
+//     * @var array<int, string>
+//     */
+//    protected $fillable = [
+//        'name',
+//        'email',
+//        'password',
+//    ];
+//
+//    /**
+//     * The attributes that should be hidden for serialization.
+//     *
+//     * @var array<int, string>
+//     */
+//    protected $hidden = [
+//        'password',
+//        'remember_token',
+//    ];
+//
+//    /**
+//     * Get the attributes that should be cast.
+//     *
+//     * @return array<string, string>
+//     */
+//    protected function casts(): array
+//    {
+//        return [
+//            'email_verified_at' => 'datetime',
+//            'password' => 'hashed',
+//        ];
+//    }
+//}
+
+namespace App\Models;
+
+// No need to use Laravel default traits and properties as
+// they're already present in the base class exactly as
+// they're defined in a default Laravel installation
+class User extends \Konekt\User\Models\User
 {
-    use HasFactory, Notifiable;
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
-    {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
-    }
 }
---
+++ config/concord.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'modules' => [
+        /**
+         * Example:
+         * VendorA\ModuleX\Providers\ModuleServiceProvider::class,
+         * VendorB\ModuleY\Providers\ModuleServiceProvider::class
+         *
+         */
+
+        Vanilo\Foundation\Providers\ModuleServiceProvider::class
+    ],
+    'register_route_models' => true
+];
