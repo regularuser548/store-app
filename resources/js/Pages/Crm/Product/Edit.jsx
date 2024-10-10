8fdc71d2d88cfb5f58d991732f6b13491c1269a1
regularuser548 <regularuser548@tutanota.com>
2024-10-10 03:29:46 +0300

    Implement product forms and validation

--- resources/js/Pages/Crm/Product/Edit.jsx
+++ resources/js/Pages/Crm/Product/Edit.jsx
@@ -2,20 +2,20 @@ import {Head, router, useForm} from "@inertiajs/react";
 import CrmMenuLayout from "@/Pages/Crm/CrmMenuLayout.jsx";
 import ProductForm from "@/Pages/Crm/Product/Components/ProductForm.jsx";
 
-export default function Edit(props) {
+export default function Edit({product, images, props}) {
 
     //https://vanilo.io/docs/4.x/products#all-product-fields
     const {data, setData, post, progress} = useForm({
-        name: props.name,
-        sku: props.sku,
-        stock: props.stock,
-        price: props.price,
-        weight: props.weight,
-        width: props.width,
-        height: props.height,
-        length: props.length,
-        description: props.description,
-        meta_keywords: props.meta_keywords,
+        name: product.name,
+        sku: product.sku,
+        stock: product.stock,
+        price: product.price,
+        weight: product.weight,
+        width: product.width,
+        height: product.height,
+        length: product.length,
+        description:product.description,
+        meta_keywords: product.meta_keywords,
 
         images: '',
         videos: '',
@@ -26,7 +26,7 @@ export default function Edit(props) {
 
     function handleSubmit(e) {
         e.preventDefault();
-        router.post(route('product.update', {product:props.product.id }), data);
+        router.post(route('product.update', {product:product.id }), data);
     }
 
     return (
@@ -36,13 +36,13 @@ export default function Edit(props) {
                 <button className='border m-2 p-1' onClick={e => router.visit(route('product.index'))}>Cancel</button>
 
                 <div id='ImageContainer'>
-                    {props.images.map((item, index) => (
-                        <img key={index} src={props.images[index]} alt='' className='w-1/6 inline'/>
+                    {images.map((item, index) => (
+                        <img key={index} src={images[index]} alt='' className='w-1/6 inline'/>
                     ))}
                 </div>
 
                 <button className='border m-2 p-1 bg-red-500'
-                        onClick={e => router.delete(route('product.destroy', {product:props.product.id }))}>Delete
+                        onClick={e => router.delete(route('product.destroy', {product:product.id }))}>Delete
                 </button>
 
         </CrmMenuLayout>
--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -4,6 +4,7 @@ import '../css/app.css';
 import { createRoot, hydrateRoot } from 'react-dom/client';
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import {StrictMode} from "react";
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -12,7 +13,7 @@ createInertiaApp({
     resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')),
     setup({ el, App, props }) {
         if (import.meta.env.DEV) {
-            createRoot(el).render(<App {...props} />);
+            createRoot(el).render(<StrictMode><App {...props} /></StrictMode>);
             return
         }
 
