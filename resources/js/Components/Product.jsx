ff582c06906486e48dcef204c09729afea0ff105
VHLAD <vhladia5301@gmail.com>
2024-12-14 23:31:55 +0200

    Fix Comment

--- resources/js/Components/Product.jsx
+++ resources/js/Components/Product.jsx
@@ -2,9 +2,15 @@ import React, { useEffect, useState } from "react";
 import { Button, message } from "antd";
 import axios from "axios";
 import { router } from "@inertiajs/react";
+import FilledHeart from "@/Icons/FilledHeart.jsx";
+import EmptyHeart from "@/Icons/EmptyHeart.jsx";
 
 export default function Product({ item, image, isCrm = false }) {
   const [isFavorite, setIsFavorite] = useState(false);
+
+  const handleAddToCart = (productId) => {
+    axios.post(route('cart.add'), {product: {id: productId}});
+  };
 
   useEffect(() => {
     // Проверяем, находится ли товар в избранном
@@ -70,17 +76,7 @@ export default function Product({ item, image, isCrm = false }) {
                 onClick={() => toggleFavorite(item.id)}
                 className="flex flex-col items-center hover:text-white"
               >
-                <svg
-                  className="w-6 h-6 mb-1"
-                  viewBox="0 0 25 25"
-                  fill={isFavorite ? "#FF8000" : "none"}
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M12.1 18.55L12 18.65L11.89 18.55C7.14 14.24 4 11.39 4 8.5C4 6.5 5.5 5 7.5 5C9.04 5 10.54 6 11.07 7.36H12.93C13.46 6 14.96 5 16.5 5C18.5 5 20 6.5 20 8.5C20 11.39 16.86 14.24 12.1 18.55ZM16.5 3C14.76 3 13.09 3.81 12 5.08C10.91 3.81 9.24 3 7.5 3C4.42 3 2 5.41 2 8.5C2 12.27 5.4 15.36 10.55 20.03L12 21.35L13.45 20.03C18.6 15.36 22 12.27 22 8.5C22 5.41 19.58 3 16.5 3Z"
-                    fill={isFavorite ? "#FF8000" : "#FFFFFF"}
-                  />
-                </svg>
+                {isFavorite ? <FilledHeart/> : <EmptyHeart/>}
               </button>
             </div>
 
