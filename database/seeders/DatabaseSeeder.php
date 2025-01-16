79c41ca5597ea0fa352c5be4b0ab7d18da52a7a2
regularuser548 <regularuser548@tutanota.com>
2025-01-16 03:14:17 +0200

    seeder update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,35 +23,35 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Admin',
             'email' => 'admin@mail.com',
-            'password' => bcrypt('admin'),
+            'password' => bcrypt('changeme'),
         ])->assignRole('admin');
 
-        //Create customer
-        User::factory()->create([
-            'name' => 'Customer',
-            'email' => 'customer@mail.com',
-            'password' => bcrypt('customer'),
-        ])->assignRole('customer');
-
-        //Create seller
-        User::factory()->create([
-            'name' => 'Seller',
-            'email' => 'seller@mail.com',
-            'password' => bcrypt('seller'),
-        ])->assignRole('seller');
-
-        //Create manager
-        User::factory()->create([
-            'name' => 'Manager',
-            'email' => 'manager@mail.com',
-            'password' => bcrypt('manager'),
-        ])->assignRole('manager');
-
-        //Create moderator
-        User::factory()->create([
-            'name' => 'Moderator',
-            'email' => 'moderator@mail.com',
-            'password' => bcrypt('moderator'),
-        ])->assignRole('moderator');
+//        //Create customer
+//        User::factory()->create([
+//            'name' => 'Customer',
+//            'email' => 'customer@mail.com',
+//            'password' => bcrypt('customer'),
+//        ])->assignRole('customer');
+//
+//        //Create seller
+//        User::factory()->create([
+//            'name' => 'Seller',
+//            'email' => 'seller@mail.com',
+//            'password' => bcrypt('seller'),
+//        ])->assignRole('seller');
+//
+//        //Create manager
+//        User::factory()->create([
+//            'name' => 'Manager',
+//            'email' => 'manager@mail.com',
+//            'password' => bcrypt('manager'),
+//        ])->assignRole('manager');
+//
+//        //Create moderator
+//        User::factory()->create([
+//            'name' => 'Moderator',
+//            'email' => 'moderator@mail.com',
+//            'password' => bcrypt('moderator'),
+//        ])->assignRole('moderator');
     }
 }
