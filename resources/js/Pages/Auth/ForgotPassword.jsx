fcc22dc192c2da89393365a97e004f31f0a5313b
ImMrDragon <crazy.little.dragon666@gmail.com>
2025-01-18 17:43:57 +0100

    Fix , Change Dising Forget pasword

--- resources/js/Pages/Auth/ForgotPassword.jsx
+++ resources/js/Pages/Auth/ForgotPassword.jsx
@@ -16,35 +16,42 @@ export default function ForgotPassword({ status }) {
     };
 
     return (
-        <GuestLayout>
-            <Head title="Forgot Password" />
+           <div className="flex items-center justify-center min-h-screen bg-[#0F0F0F]">
+             <Head title="Forgot Password" />
 
-            <div className="mb-4 text-sm text-gray-600 dark:text-gray-400">
-                Forgot your password? No problem. Just let us know your email address and we will email you a password
-                reset link that will allow you to choose a new one.
-            </div>
 
-            {status && <div className="mb-4 font-medium text-sm text-green-600 dark:text-green-400">{status}</div>}
+             {status && <div className="mb-4 font-medium text-sm text-green-600 dark:text-green-400">{status}</div>}
 
-            <form onSubmit={submit}>
-                <TextInput
-                    id="email"
-                    type="email"
-                    name="email"
-                    value={data.email}
-                    className="mt-1 block w-full"
-                    isFocused={true}
-                    onChange={(e) => setData('email', e.target.value)}
-                />
-
-                <InputError message={errors.email} className="mt-2" />
-
-                <div className="flex items-center justify-end mt-4">
-                    <PrimaryButton className="ms-4" disabled={processing}>
-                        Email Password Reset Link
-                    </PrimaryButton>
-                </div>
-            </form>
-        </GuestLayout>
+             <div className="mb-4 w-full flex justify-center items-center">
+               <form onSubmit={submit} className="flex flex-col items-center w-full max-w-md bg-[#0F0F0F] text-white">
+                 <label htmlFor="password" className="block font-medium text-gray-200 text-xl">
+                   Введіть ваш емайл для відновлення пароля
+                 </label>
+                 <input
+                   id="email"
+                   type="email"
+                   name="email"
+                   value={data.email}
+                   className="mt-2 block w-full bg-[#1E1E1E] border border-[#FFFFFF] rounded-md p-2 text-[#FFFFFF] focus:ring-2 focus:ring-orange-500 focus:outline-none"
+                   onChange={(e) => setData('email', e.target.value)}
+                 />
+                 <InputError message={errors.email} className="mt-2"/>
+                 {/*<div className="w-full ">*/}
+                 {/*  <PrimaryButton className="bg-orange-500 hover:bg-orange-600 text-center text-[#0F0F0F] font-bold py-2 px-4 rounded mt-10 block w-full " disabled={processing}>*/}
+                 {/*    Відновити пароль*/}
+                 {/*  </PrimaryButton>*/}
+                 {/*</div>*/}
+                 <div className="w-full">
+                   <button
+                     type="submit"
+                     className="bg-orange-500 hover:bg-orange-600 text-[#0F0F0F] font-bold py-2 px-4 rounded mt-3 block w-full "
+                     disabled={processing}
+                   >
+                     Відновити пароль
+                   </button>
+                 </div>
+               </form>
+             </div>
+           </div>
     );
 }
